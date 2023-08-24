0163275cf1f3bdd73300eaecee82ca7e33882082
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-08-24 13:38:24 +0200

    Add files via upload

---
+++ variables_and_basics/cuccmok.js
@@ -0,0 +1,40 @@
+console.log("szeretem a wotot");
+console.log("utálom a matekot");
+
+window.alert("Attention you might be not pay your internet bill! PAY IMMEDIATELY");
+
+// ez egy komment
+
+/*
+sfsfsdf
+dsfdsfd
+sdfdsf
+fdsfsf
+ez egy több soros comment*/
+
+let eletkor;  // változo megadása
+eletkor = 20;  // változoba most bele is rakunk valamit hát ja ez nem pythonos ejnye :(
+
+let cuccmok = 69; // lehet egybe is hoppá!
+
+let keresztnev = "Lócaj";
+
+let lebegopontos = 3.6
+
+let kasztraltatva = true;
+
+cuccmok = cuccmok + 1; // uuuu meglehet növelni a változo értékét mint pythonban azztaaaa
+
+cuccmok += 1;  // szám
+
+
+
+console.log("Lócaj jelenleg ", eletkor, " éves!");
+console.log("funny number goo brrrr ",cuccmok);
+console.log("Dicsértessék ",keresztnev);
+console.log(lebegopontos," Roentgen Not Great Not Terrible");
+console.log("kasztrátatva: ",kasztraltatva);
+
+document.getElementById("p1").innerHTML = "Üdv " + keresztnev;  // össze kötjük a HTLM-ben szereplő id="p1" változos elemmel és belerakunk stringet meg egy js változot anélkül hogy htmlben nyulkálnánk
+document.getElementById("p2").innerHTML = "Te " + eletkor + " éves lehetsz just maybe";
+document.getElementById("p3").innerHTML = lebegopontos + " Roentgen Not Great Not Terrible";
