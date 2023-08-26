c1238b3569d0442ed19ab2a63c6c20fe7d93d19e
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-08-26 18:54:58 +0200

    Add files via upload

---
+++ oparatorok/cuccmok.js
@@ -0,0 +1,24 @@
+
+
+let diak = 20;
+
+// diak = diak - 1;
+
+// diak = diak - 1;
+
+// diak = diak * 2;
+
+// diak = diak / 2;
+
+let extra_diakok = diak % 3;
+
+
+diak -= 1 // lehet röviditeni mint pythonban 
+
+console.log(diak);
+console.log(extra_diakok);
+
+
+muveleti_sorrend = 3 + 3 - 6 * (64**0.5) // müveleti sorrend ugyanugy megvan itt is mint a matekban 
+
+console.log(muveleti_sorrend)
